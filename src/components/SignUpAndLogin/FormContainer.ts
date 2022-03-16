083af5dad50e537ea10a7a870e41715c8670c046
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-03-16 16:32:49 -0700

    Created some styled components to use for my Login and Signup components

---
+++ src/components/SignUpAndLogin/FormContainer.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const FormContainer = styled.div`
+  display: flex;
+  padding: 2rem;
+  flex-flow: column nowrap;
+  gap: 1rem;
+`;
+
+export default FormContainer;
---
+++ src/components/SignUpAndLogin/Heading.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  font-family: 2.5rem;
+  font-family: "Dancing Script", cursive;
+  color: ${({ theme }) => theme.palette.common.black};
+`;
+
+export default Heading;
