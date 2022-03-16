7d521877ff52b6c5a28807ed78a84031595b664c
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-03-15 19:19:24 -0700

    Created my theme object which I will use throughout my app for styling, and exported the interfaces for the theme object by declaring the styled-components module with the interfaces in it

---
+++ src/Themes/Theme.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from "styled-components";
+
+const Theme: DefaultTheme = {
+  palette: {
+    primary: {
+      main: "FFFFFF",
+      contrastText: "#000000",
+    },
+    secondary: {
+      main: "#0095f6",
+      contrastText: "#FFFFFF",
+    },
+    lightRed: "#efefef",
+    neutral: "#ed4956",
+  },
+  primaryFont: `Montserrat sans-serif`,
+};
+
+export default Theme;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="react-scripts" />
+
+interface IPallette {
+  main: string;
+  contrastText: string;
+}
+declare module "styled-components" {
+  export interface DefaultTheme {
+    palette: {
+      primary: IPallette;
+      secondary: IPallette;
+      lightRed: string;
+      neutral: string;
+    };
+    primaryFont: string;
+  }
+}
