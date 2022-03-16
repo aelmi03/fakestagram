5ecd9df34caf00264540c0a952d2cca26e41d232
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-03-15 23:45:18 -0700

    Created the basiclogic of my userSlice in the features folder

---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+
+type User = {
+  name: string;
+};
+const initialUser: User = {
+  name: "placeholder",
+};
+export const userSlice = createSlice({
+  name: "user",
+  initialState: initialUser,
+  reducers: {
+    setUser(state, action: PayloadAction<User>) {
+      state = action.payload;
+    },
+  },
+});
+
+export const selectUser = (state: RootState) => state.user;
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;
