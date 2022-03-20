47fad8564b4c372f49c88fd2b6bfc9ee966abd3a
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-03-20 15:32:20 -0700

    Added the functionality for the App component to either render the newly created and currently non-functional Main component or the SignUpAndLogin component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import SignUpAndLogin from "./components/SignUpAndLogin/";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
+import Main from "./components/Main";
 
 function App() {
+  const [authUser, loading] = useAuthState(getAuth());
+  if (!loading) {
+    return <Main />;
+  }
   return <SignUpAndLogin />;
 }
 
