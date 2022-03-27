2807e5805f49afab04c04b2464aefe1e14522a4b
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-03-27 00:56:47 -0700

    Finished the functionality for fetching the specific posts for the user and showing it

--- src/components/Main/Profile/ProfilePosts.tsx
+++ src/components/Main/Profile/ProfilePosts.tsx
@@ -5,17 +5,42 @@ import { FaRegBookmark } from "react-icons/fa";
 import Post from "../../utils/PostInterface";
 import React, { useEffect, useState } from "react";
 import { User } from "../../../features/user/userSlice";
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import { Unsubscribe } from "firebase/auth";
 interface IProps {
   profileUser: User;
 }
 const ProfilePosts = ({ profileUser }: IProps) => {
-  console.log(profileUser, "Profile Posts :)");
   const [typeOfPosts, setTypeOfPosts] = useState("Own Posts");
   const [profilePosts, setProfilePosts] = useState<Post[]>([]);
   useEffect(() => {
+    const getProfileUserPosts = async () => {
+      const profileUserPostsQuery = query(
+        collection(getFirestore(), "posts"),
+        where("postedBy", "==", `${profileUser.id}`)
+      );
+      return onSnapshot(profileUserPostsQuery, (snapshot) => {
+        const posts = snapshot.docs.map((doc) => doc.data() as Post);
+        setProfilePosts(posts);
+      });
+    };
+    let unsubscribeFunction: Unsubscribe;
     if (typeOfPosts === "Own Posts") {
+      getProfileUserPosts().then((unsubscribe) => {
+        unsubscribeFunction = unsubscribe;
+      });
     }
-  }, [typeOfPosts]);
+    return () => {
+      unsubscribeFunction();
+    };
+  }, [typeOfPosts, profileUser.id]);
   return (
     <ProfilePostsWrapper>
       <FlexContainer direction="row" justifyContent="space-evenly">
@@ -26,7 +51,11 @@ const ProfilePosts = ({ profileUser }: IProps) => {
           <FaRegBookmark /> <InfoText>SAVED POSTS</InfoText>
         </InfoContainer>
       </FlexContainer>
-      <PostsContainer>{}</PostsContainer>
+      <PostsContainer>
+        {profilePosts.map((post) => (
+          <ProfilePost src={post.imgSrc} alt="profile" />
+        ))}
+      </PostsContainer>
     </ProfilePostsWrapper>
   );
 };
@@ -41,11 +70,11 @@ const ProfilePostsWrapper = styled.div`
 `;
 const PostsContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, 30.5%);
+  grid-template-columns: repeat(auto-fit, calc(97% / 3));
   width: 100%;
-  gap: 0.6rem;
-  justify-content: center;
+  gap: 0.4rem;
   @media only screen and (min-width: 768px) {
+    grid-template-columns: repeat(auto-fit, calc(94% / 3));
     gap: 2.8rem;
   }
 `;
@@ -53,7 +82,11 @@ const InfoText = styled.p`
   font-family: ${({ theme }) => theme.primaryFont};
   font-size: 1.3rem;
 `;
-
+const ProfilePost = styled.img`
+  width: 100%;
+  height: 100%;
+  aspect-ratio: 1 / 1;
+`;
 const InfoContainer = styled.div`
   display: flex;
   flex-flow: row nowrap;
