61a6a047d26b03eaefd46b3dcfdba3519fd23658
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-04-03 15:47:17 -0700

    Wrote the tests for my PostModal component to make sure it rendered the correct children components based on the width prop passed to it

---
+++ src/tests/Main/Posts/PostModal.test.tsx
@@ -0,0 +1,68 @@
+import PostModal from "../../../components/Main/Posts/PostModal";
+import { ThemeProvider } from "styled-components";
+import Theme from "../../../Themes/Theme";
+import { render, screen } from "@testing-library/react";
+import Post from "../../../components/utils/PostInterface";
+import { User } from "../../../features/user/userSlice";
+import { Timestamp } from "firebase/firestore";
+import SmallModal from "../../../components/Main/Posts/SmallModal";
+import LargeModal from "../../../components/Main/Posts/LargeModal";
+
+const changeModalStatus = jest.fn();
+const changePostToShow = jest.fn();
+const mockPost: Post = {
+  postedBy: "randomID",
+  comments: [],
+  likes: [],
+  caption: "Went on a trip!",
+  timestamp: "faketimestamp" as unknown as Timestamp,
+  id: "fakePostID",
+  imgSrc: "fakeImgSrc",
+};
+const mockUser: User = {
+  fullName: "John Doe",
+  username: "johnDoe23",
+  following: [],
+  savedPosts: [],
+  id: "fakeUserID",
+  profilePicture: "path/to/photo/for/johnDoe23",
+  biography: "Love riding bicycles and going to the beach :)",
+};
+jest.mock("../../../components/Main/Posts/SmallModal", () => {
+  return () => <div>Small Modal Component</div>;
+});
+jest.mock("../../../components/Main/Posts/LargeModal", () => {
+  return () => <div>Large Modal Component</div>;
+});
+describe("Post Modal Component", () => {
+  it("Renders the SmallModal component if the width is smaller than 768px", () => {
+    render(
+      <ThemeProvider theme={Theme}>
+        <PostModal
+          post={mockPost}
+          postUser={mockUser}
+          changeModalStatus={changeModalStatus}
+          changePostToShow={changePostToShow}
+          width={767}
+        />
+      </ThemeProvider>
+    );
+    expect(screen.getByText("Small Modal Component")).toBeInTheDocument();
+    expect(screen.queryByText("Large Modal Component")).not.toBeInTheDocument();
+  });
+  it("Renders the LargeModal component if the width is greater than or equal to 768px", () => {
+    render(
+      <ThemeProvider theme={Theme}>
+        <PostModal
+          post={mockPost}
+          postUser={mockUser}
+          changeModalStatus={changeModalStatus}
+          changePostToShow={changePostToShow}
+          width={768}
+        />
+      </ThemeProvider>
+    );
+    expect(screen.getByText("Large Modal Component")).toBeInTheDocument();
+    expect(screen.queryByText("Small Modal Component")).not.toBeInTheDocument();
+  });
+});
