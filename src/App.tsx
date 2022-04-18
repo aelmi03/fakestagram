2745513c28bcfe26ea6b2e17f912acad811b0356
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-04-18 03:46:33 -0700

    Added the functionality for initializing the chat rooms by getting the chats from the firestore database when the user signs in

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SignUpAndLogin from "./components/SignUpAndLogin/";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { getAuth } from "firebase/auth";
 import Main from "./components/Main";
-import { doc, getFirestore, onSnapshot } from "firebase/firestore";
-import { setUser, User } from "./features/user/userSlice";
-import { useAppDispatch } from "./app/hooks";
+import {
+  collection,
+  doc,
+  getFirestore,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import { selectUser, setUser, User } from "./features/user/userSlice";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { Chat, setChats } from "./features/chatRoom/chatRoomSlice";
 
 function App() {
   console.log("APP");
   const [authUser] = useAuthState(getAuth());
+  const user = useAppSelector(
+    selectUser,
+    (prevUser, nextUser) => prevUser.id === nextUser.id
+  );
   const dispatch = useAppDispatch();
   const initializeUser = async () => {
     const userDoc = doc(getFirestore(), `users/${authUser!.uid}`);
@@ -17,6 +29,22 @@ function App() {
       dispatch(setUser(snapshot.data() as User));
     });
   };
+  useEffect(() => {
+    const initializeChatRooms = async () => {
+      const chatRoomsQuery = query(
+        collection(getFirestore(), "chatRooms"),
+        where("members", "array-contains", `${user.id}`)
+      );
+      onSnapshot(chatRoomsQuery, (snapshot) => {
+        const chats = snapshot.docs.map((doc) => doc.data() as Chat);
+        console.log("chatss", chats);
+        dispatch(setChats(chats));
+      });
+    };
+    if (user.id) {
+      initializeChatRooms();
+    }
+  }, [user]);
   if (authUser) {
     initializeUser();
     return <Main />;
